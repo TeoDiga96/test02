e215e7ff193b31e35c44a30b26452ea13d4aaaf1
TeoDiga96 <matteo.digallo96@outlook.com>
2025-04-24 17:54:47 +0200

    basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,16 +12,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.deepPurple,
+        ),
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.green,
         ),
         primarySwatch: Colors.green,
       ),
-
       home: const RootPage(),
     );
   }
 }
+
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
 
@@ -29,10 +34,30 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int currenPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('test app 2'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('bottone premuto');
+        },
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currenPage = index;
+          });
+        },
+        selectedIndex: currenPage,
+      ),
     );
   }
 }
