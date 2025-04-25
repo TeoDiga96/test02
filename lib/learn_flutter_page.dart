5944681218f6bce2e644b45bf0a926f9156ffae4
TeoDiga96 <matteo.digallo96@outlook.com>
2025-04-25 16:45:23 +0200

    roba varia

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,8 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool isCheckbox = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +22,136 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back),
         ),
-      ),
-      body: Column(
-        children: [
-          Image.asset('images/deadpool.png'),
-          const SizedBox(height: 10,),
-          const Divider(
-            color: Colors.red,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('stocazzo');
+            },
+            icon: Icon(Icons.info_outline),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/deadpool.png'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text(
+                  'text widget',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isSwitch ? Colors.blue : Colors.deepOrange,
+                  ),
+                  onPressed: () {
+                    print('ciao Logan');
+                  },
+                  child: Text('saluta 1'),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    print('ciao Hugh');
+                  },
+                  child: Text('saluta 2'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print('ciao Wolverine');
+                  },
+                  child: Text('saluta 3'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () {
+                    print('chiamo i pompieri');
+                  },
+                  child: const Icon(
+                    Icons.local_fire_department,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.local_police,
+                    color: Colors.blue,
+                  ),
+                  onTap: () {
+                    print('chiama gli sbirri');
+                  },
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool!;
+                  });
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.red,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Image.network(
+                'https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/07/hugh-jackman-recreating-wolverine-meme.jpg'),
+          ],
+        ),
       ),
     );
   }
